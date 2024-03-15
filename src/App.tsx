1015ef4a726d6012090c8beddbaaf438b3f6da5b
SutuSebastian <sebiitv@gmail.com>
2024-03-15 21:46:00 +0200

    chore: update packages; add prettier; configure linting and formatting for tailwind and `flowbite-react`; cleanup boilerplate files; add vscode config files; format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DarkThemeToggle } from "flowbite-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="flex min-h-screen items-center justify-center gap-2 dark:bg-gray-800">
+      <h1 className="text-2xl dark:text-white">
+        Flowbite React + Create React App
+      </h1>
+      <DarkThemeToggle />
+    </main>
   );
 }
 
